586ea93b4488d0998b7f93a36a291ff91986db86
Yoshimasa Tanabe <lanabe.lanabe@gmail.com>
2012-10-06 18:53:21 +0900

    initial data added.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,4 @@
 -- You can use this file to load seed data into the database using SQL statements
-insert into Member (id, name, email, phone_number) values (0, 'John Smith', 'john.smith@mailinator.com', '2125551212') 
+INSERT INTO todo (content, _order, done, user_id) VALUES ('test1', 1, false, 1);
+INSERT INTO todo (content, _order, done, user_id) VALUES ('test2', 2, true,  2);
+INSERT INTO todo (content, _order, done, user_id) VALUES ('test3', 3, false, 3);
